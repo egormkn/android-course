32b16584e341a83c5c8a7322b0e5c633c6cc7102
Dmitry Trunin <dtrounine@yandex.ru>
2016-11-28 13:23:54 +0300

    Update Movie.java

--- app/src/main/java/ru/ifmo/droid2016/tmdb/model/Movie.java
+++ app/src/main/java/ru/ifmo/droid2016/tmdb/model/Movie.java
@@ -12,7 +12,7 @@ public class Movie {
     /**
      * Path изображения постера фильма. Как из Path получить URL, описано здесь:
      *
-     * https://developers.themoviedb.org/3/getting-started/languages
+     * https://developers.themoviedb.org/3/getting-started/images
      *
      * В рамках ДЗ можно не выполнять отдельный запрос /configuration, а использовать
      * базовый URL для картинок: http://image.tmdb.org/t/p/ и
